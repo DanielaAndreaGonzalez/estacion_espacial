63d05e8604a52dbd4ce7787a5172b835ce6d99df
DanielaAndreaGonzalez <daniela.andrea.gonzalez@correounivalle.edu.co>
2022-07-12 21:12:50 -0500

    Cross solucion

--- src/main/java/com/sofkaU/estacion_espacial/controlador/ControladorVehiculoLanzadera.java
+++ src/main/java/com/sofkaU/estacion_espacial/controlador/ControladorVehiculoLanzadera.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,6 +33,7 @@ import com.sofkaU.estacion_espacial.modelo.VehiculoLanzadera;
  */
 @RestController
 @RequestMapping("/naves")
+@CrossOrigin("http://localhost:8080/")
 public class ControladorVehiculoLanzadera {
 	
 	@Autowired
